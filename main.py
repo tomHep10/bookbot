b8ca8b1e5e5f461c608b25be7a94de624276f9a6
ThomasHeeps <thomasheeps10@gmail.com>
2024-12-21 21:59:36 -0500

    created char_duplicates function

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ def main():
   with open(path , 'r') as f:
     file_contents = full_text(path)
     word_count = count_words(file_contents)
-    print(word_count)
+    dup_chars = char_duplicates(file_contents)
 
 def full_text(path):
-    with open(path) as f:
-        return f.read()
+  with open(path) as f:
+      return f.read()
 
 def count_words(file_contents):
-  with open(path) as f:
-    words = file_contents.split()
-    print(len(words))
+  words = file_contents.split()
+  print(len(words))
+
+def char_duplicates(file_contents):
+  file_contents = file_contents.lower()
+  count = {}
+  for c in file_contents:
+    if c not in count:
+      count[c] = 1
+    else:
+      count[c] += 1
+  return count
 
 if __name__ == '__main__':
   main()
